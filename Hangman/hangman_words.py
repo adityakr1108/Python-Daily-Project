a573a28be6c7452c66dc786de7dcc6d73d0eceea
Aditya Kumar <aditya.kr.agrawal1108@gmail.com>
2024-10-21 23:40:29 +0530

    Update hangman_words.py

--- Hangman/hangman_words.py
+++ Hangman/hangman_words.py
@@ -1,9 +1,10 @@
 word_list = [
     'about', 'above', 'after', 'again', 'below', 'could', 'every', 'first', 'found', 'great', 
     'house', 'large', 'learn', 'never', 'other', 'place', 'plant', 'right', 'small', 'sound', 
-    'their', 'these', 'thing', 'think', 'water', 'where', 'which', 'world', 'would', 'write'
+    'their', 'these', 'thing', 'think','season', 'water', 'where', 'which', 'world', 'would', 'write'
     'kilobyte', 'knapsack', 'larynx', 'marquis', 'megahertz', 'nightclub', 'oxidize', 
     'peekaboo', 'pneumonia', 'polka', 'quixotic', 'razzmatazz', 'rickshaw', 'schnapps',
     'shiv', 'snazzy', 'stronghold', 'triphthong', 'unknown', 'unworthy', 'vodka', 'voodoo',
     'wristwatch', 'wyvern', 'xylophone', 'yachtsman', 'zigzagging', 'zilch', 'zipper'
+    'zebra'
 ]
