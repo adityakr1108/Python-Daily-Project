54ec2f2b75a55f879e21204dd858cd3a832132c3
Aditya Kumar <aditya.kr.agrawal1108@gmail.com>
2024-10-24 01:53:24 +0530

    Some update in the main.py

--- US_State.py/main.py
+++ US_State.py/main.py
@@ -31,6 +31,11 @@ while game_is_on:
     input_state = screen.textinput(title="Guess the U.S. State", prompt="What's another state name?")
     if input_state is None:  # If the user cancels the input dialog
         game_is_on = False
+        turtle_new.hideturtle()
+        turtle_new.goto(0, 220)  # Adjust the y-coordinate as needed to position the text at the top
+        turtle_new.write(f"Good Bye, you have scored {score}. Thanks for playing..", align="center", font=("Arial", 16, "bold"))
+        time.sleep(3)
+        screen.bye()
         break
 
     found_state = False
@@ -41,25 +46,29 @@ while game_is_on:
             ycor = row["y"]
             state_name = row["state"]
             turtle_new.goto(xcor, ycor)
-            turtle_new.write(state_name)
+            turtle_new.write(state_name,font=("Courier", 12, "bold"))
             found_state = True
             break
 
     if not found_state:
         chance -= 1
         if chance > 0:
-            time.sleep(0.5)
+            time.sleep(0.8)
             another_chance = screen.textinput(title=f"Do you want to try again? {chance} left", prompt="Yes or No").lower()
             if another_chance == "yes":
                 game_is_on = True
             else:
                 game_is_on = False
-                turtle_new.write(f"Good Bye, you have scored {score}. Thanks for playing..",align="center", font=("Arial", 16, "bold"))
-                time.sleep(0.5)
+                turtle_new.hideturtle()
+                turtle_new.goto(0, 220)  # Adjust the y-coordinate as needed to position the text at the top
+                turtle_new.write(f"Good Bye, you have scored {score}. Thanks for playing..", align="center", font=("Arial", 16, "bold"))
+                time.sleep(3)
                 screen.bye()
         else:
             game_is_on = False
-            turtle_new.write(f"Good Bye, you have scored {score}. Thanks for playing..",align="center", font=("Arial", 16, "bold"))
+            turtle_new.hideturtle()
+            turtle_new.goto(0, 220)  # Adjust the y-coordinate as needed to position the text at the top
+            turtle_new.write(f"Good Bye, you have scored {score}. Thanks for playing..", align="center", font=("Arial", 16, "bold"))
             time.sleep(3)
             screen.bye()
 screen.mainloop()
